5b04bb54b462720770acb7456caeeea0f0a2ffee
mbodenhamer <mbodenhamer@mbodenhamer.com>
2017-11-02 23:38:44 -0500

    Adding examples

--- tests/test_helpers.bash
+++ tests/test_helpers.bash
@@ -3,5 +3,5 @@ TEST_FILE=$(basename $BATS_TEST_FILENAME .bats)
 TEST_DIR=$(dirname $BATS_TEST_FILENAME)
 
 function launch {
-    docker run --rm -it -v /tmp:/tmp -v `pwd`:/app mbodenhamer/alpine-python:latest "$@"
+    docker run --rm -it -v `pwd`:/app -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp mbodenhamer/alpine-python:latest "$@"
 }
